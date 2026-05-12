c84859bc992f7135b1852cc7a9c97ae07e79da27
leticia_abraao <leticiaaod@gmail.com>
2026-05-12 19:23:49 -0300

    first commit

---
+++ src/main/java/com/project/lendAHand/LendAHandApplication.java
@@ -0,0 +1,13 @@
+package com.project.lendAHand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendAHandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendAHandApplication.class, args);
+	}
+
+}
